7be4f1cd06a8c782d95a740d7c072cd0ae43ee62
Dave Freilich <dave@wundergraph.com>
2024-09-03 18:39:58 +0300

    Add second server, create cosmos config

---
+++ user_age_server/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/df-wg/graphql-test-server/user_age_server/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	UserList []*model.User
+}
